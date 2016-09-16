f0dde7590d5a3bc9299bb3c61a637b9229713589
Christian <cbenseler@gmail.com>
2016-09-16 10:21:04 -0300

    prepublish task

--- src/ConfirmModal.js
+++ src/ConfirmModal.js
@@ -1,3 +1,62 @@
+class ConfirmModal {
 
+	constructor(opts) {
+		this.opts = opts;
 
-module.exports = require("./ConfirmModalClass").default;
+		let prefix = this.opts.id_prefix ? this.opts.id_prefix : "mm-confirmmodal"
+		;
+		this.ids = {
+			btn_cancel:  prefix + "-cancel",
+			btn_proceed: prefix + "-proceed",
+			container: prefix,
+			overlay: prefix + "-overlay"
+		};
+	}
+
+	open() {
+		if(!document.getElementById(this.ids.container)) 
+			$("body").append(`<div id='${this.ids.container}'>
+								<div id='${this.ids.container}-content'>
+									<h2>${this.opts.messages.title}</h2>
+									<p>${this.opts.messages.desc}</p>
+									<div class="pull-right">
+									<button class="btn btn-primary" id="${this.ids.btn_cancel}">${this.opts.messages.cancel}</button>
+									<button class="btn btn-primary" id="${this.ids.btn_proceed}">${this.opts.messages.proceed}</button>
+									</div>
+								</div>
+							  </div>
+							  <div id='mm-confirmmodal-overlay'></div>`);
+
+		this.modalcontainer = document.getElementById(this.ids.container);
+		this.modaloverlay = document.getElementById(this.ids.overlay);
+
+		this.proceed = document.getElementById(this.ids.btn_proceed);
+		this.cancel = document.getElementById(this.ids.btn_cancel);
+
+		this.handlers();
+	}
+
+	handlers() {
+		this.proceed.onclick = event => {
+			event.stopPropagation();
+			if(this.opts.onProceed) {
+				this.opts.onProceed(event);
+			}
+			this.closeMe();
+		}
+
+		this.cancel.onclick = event => {
+			event.stopPropagation();
+			if(this.opts.onCancel)
+				this.opts.onCancel(event);
+			this.closeMe();
+		}
+	}
+
+	closeMe() {
+		$(this.modalcontainer).remove();
+		$(this.modaloverlay).remove();
+	}
+}
+
+module.exports = ConfirmModal;
