064d61015fcbaca1980d402bfa0335678de37b58
Christian <cbenseler@gmail.com>
2016-09-21 09:59:12 -0300

    Units tests for options

--- test/unit/ConfirmModal.spec.js
+++ test/unit/ConfirmModal.spec.js
@@ -14,6 +14,42 @@ describe('ConfirmModal (unit)', () => {
 		})
 	})
 
+	describe('when instantiated', () => {
+		it('should return an object', () => {
+			expect(new ConfirmModal()).to.be.an('object')
+		})
+	})
+
+	describe('when passed callback functions', () => {
+		
+		let modal = new ConfirmModal({
+			onProceed: () => {},
+			onCancel: () => {}
+		})
+
+		it('onProceed should be a function', () => {
+			expect(modal.callbacks.onProceed).to.be.an('function')
+		})
+
+		it('onCancel should be a function', () => {
+			expect(modal.callbacks.onCancel).to.be.an('function')
+		})
+	})
+
+	describe('when passed prefix to ids', () => {
+		
+		let modal = new ConfirmModal({
+			id_prefix: "customprefix"
+		})
+
+		it('prefix should be customprefix', () => {
+			expect(modal.ids.container).to.be.equal('customprefix')
+		})
+
+	})
+
+	
+
 	afterEach(() => {
 		nock.cleanAll()
 	})
