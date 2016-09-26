aab5ecf486e1eccfc839cbf9e56a89d0d22b7f2e
Christian <cbenseler@gmail.com>
2016-09-26 14:47:02 -0300

    Setting up cloud service

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,4 +1,4 @@
-exports.config = {
+var config = {
     
     //
     // ==================
@@ -102,7 +102,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    // services: [],//
+    // services: ["phantomjs"],//
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
     // see also: http://webdriver.io/guide/testrunner/frameworks.html
@@ -114,7 +114,7 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: http://webdriver.io/guide/testrunner/reporters.html
-    // reporters: ['dot'],
+    reporters: ['spec'],
     //
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
@@ -186,3 +186,11 @@ exports.config = {
     // onComplete: function(exitCode) {
     // }
 }
+
+if (process.env.CI) {
+    config.capabilities = [{
+        browserName: 'chrome'
+    }]
+}
+
+exports.config = config;
