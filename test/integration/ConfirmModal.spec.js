af33ff609bed0bea3147f86aad9d776fe224a209
Christian <cbenseler@gmail.com>
2016-09-26 13:09:40 -0300

    Use chain as promise in integration test

--- test/integration/ConfirmModal.spec.js
+++ test/integration/ConfirmModal.spec.js
@@ -1,17 +1,18 @@
+let chai = require('chai')
+let chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
 
 describe('ConfirmModal.js html test page', () => {
 
 	let nav = browser.url(`file://${__dirname}/index.spec.html`)
 
-    it('should have the right title', () => {
-        var title = browser.getTitle()
-        expect(title).to.be.equal('ConfirmModal.js Example');
+	it('should have the right title', () => {
+		expect(browser.getTitle()).to.be.equal('ConfirmModal.js Example');
+
 	})
 
-    it('should have confirm modal element', () => {
-		return nav.click('#thisbtn').isExisting("#mm-confirmmodal").then( isExisting => {
-			expect(isExisting).to.be.true
-		})
+	it('should have confirm modal element', () => {
+		return nav.click('#thisbtn').isExisting("#mm-confirmmodal").should.eventually.true
     })
 		
 })
