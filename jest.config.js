f5dd6a27c5b03f95bf9ddc9983a8bf48ad67b0ec
Chris Benseler <cbenseler@gmail.com>
2021-03-10 09:10:16 -0300

    chore: coverage config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,17 @@
 module.exports = {
-    verbose: true,
-    testMatch: ['**/?(*.)+(spec|test).js'],
-    roots: ["<rootDir>/"],
-    collectCoverage: true,
-    moduleFileExtensions: ["js", "mjs"],
-    transform: {
-      "^.+\\.js$": "babel-jest",
-      "^.+\\.mjs$": "babel-jest",
-    },
-  };
+  verbose: true,
+  testMatch: ["**/?(*.)+(spec|test).js"],
+  roots: ["<rootDir>/"],
+  collectCoverage: true,
+  moduleFileExtensions: ["js", "mjs"],
+  transform: {
+    "^.+\\.js$": "babel-jest",
+    "^.+\\.mjs$": "babel-jest",
+  },
+  collectCoverageFrom: [
+    "src/*.{js,jsx}",
+    "!**/node_modules/**",
+    "!**/vendor/**",
+  ],
+  coverageDirectory: "./coverage/"
+};
